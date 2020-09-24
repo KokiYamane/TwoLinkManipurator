a33ca24093676d71e89290a6c77e63e0c6327990
yamaneco <ykouki111228@gmail.com>
2020-09-25 04:34:22 +0900

    Add path planner

--- manipurator.py
+++ manipurator.py
@@ -1,4 +1,6 @@
 import math
+import numpy as np
+import matplotlib.pyplot as plt
 
 class Manipurator():
     def __init__(self, l1, l2, d0=0):
@@ -6,20 +8,91 @@ class Manipurator():
         self.l2 = l2
         self.d0 = d0
 
-    def inverseKinematics(self, x, y):
-        theta1 = math.atan2(y - self.d0, x + 1e-10) + \
-            math.acos((self.l1**2 + x**2 + (y - self.d0)**2-self.l2**2) / \
-            (2 * self.l1 * math.sqrt(x**2+(y-self.d0)**2) + 1e-10))
-        theta2 = math.pi - math.acos(
-            (self.l1**2 + self.l2**2 - x**2 - (y - self.d0)**2) /
-            (2 * self.l1 * self.l2))
+    def inverseKinematics(self, x, y, eps=1e-10):
+        tmp = (self.l1**2 + x**2 + (y - self.d0)**2 - self.l2**2) / \
+            (2 * self.l1 * math.sqrt(x**2 + (y - self.d0)**2) + eps)
+        tmp = np.clip(tmp, -1, 1)
+        theta1 = math.atan2(y - self.d0, x + eps) - math.acos(tmp)
+
+        tmp2 = (self.l1**2 + self.l2**2 - x**2 - (y - self.d0)**2) / \
+            (2 * self.l1 * self.l2)
+        tmp2 = np.clip(tmp2, -1, 1)
+        theta2 = math.pi - math.acos(tmp2)
+
         return theta1, theta2
+
+class PathPlanner():
+    def __init__(self, acc, vmax, dt=0.01):
+        self.acc = acc
+        self.vmax = vmax
+        self.dt = dt
+
+    def distance(self, x1, y1, x2, y2):
+        return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+        
+    def angle(self, x1, y1, x2, y2):
+        return math.atan2(y2 - y1, x2 - x1)
+
+    def planning(self, x1, y1, x2, y2):
+        angle = self.angle(x1, y1, x2, y2)
+        print(math.degrees(angle))
+        print(self.vmax ** 2 / self.acc / 2)
+
+        t, v = 0, 0
+        x, y = x1, y1
+        diff = self.distance(x1, y1, x2, y2)
+        diff0 = diff
+        distance = 0
+
+        tList, xList, yList, vList = [0], [x1], [y1], [0]
+        while diff > 0.1:
+            t += self.dt
+            if t > 2: break
+            print(diff0 - distance)
+
+            if diff0 - distance < self.vmax ** 2 / self.acc / 2:
+                v -= self.acc * self.dt
+            elif v < self.vmax:
+                v += self.acc * self.dt
+            if v < 0: break
+            v = np.clip(v, 0, self.vmax)
+
+            x += v * self.dt * math.cos(angle)
+            y += v * self.dt * math.sin(angle)
+            diff = self.distance(x, y, x2, y2)
+            distance += v * self.dt
+
+            tList.append(t)
+            xList.append(x)
+            yList.append(y)
+            vList.append(v)
+
+            print(t, x, y, diff, v)
+
+        return {'t': tList, 'x': xList, 'y': yList, 'v': vList}
 
 if __name__ == '__main__':
     manipurator = Manipurator(150, 150)
-    xList = range(100)
-    yList = range(100)
+    vmax = 308.7904831758910
+    acc = vmax / 0.2
+    path = PathPlanner(acc, vmax).planning(300, 0, 155, 200)
 
-    for x, y in zip(xList, yList):
-        theta1, theta2 = manipurator.inverseKinematics(x, y)
-        print(x, y, theta1, theta2)
+    theta1List, theta2List = [], []
+    for i in range(len(path['t'])):
+        theta1, theta2 = manipurator.inverseKinematics(path['x'][i], path['y'][i])
+        theta1List.append(math.degrees(theta1))
+        theta2List.append(math.degrees(theta2))
+
+    theta1 = np.stack([path['t'], theta1List], axis=1)
+    theta2 = np.stack([path['t'], theta2List], axis=1)
+    np.savetxt('theta1.csv', theta1, delimiter=',', fmt='%04f')
+    np.savetxt('theta2.csv', theta2, delimiter=',', fmt='%04f')
+
+    plt.figure()
+    plt.plot([300, 155], [0, 200])
+    plt.plot(path['x'], path['y'])
+    plt.savefig('path.png')
+
+    plt.figure()
+    plt.plot(path['t'], path['v'])
+    plt.savefig('vel.png')
