17a4fb1b0d44315ec74c6120e9221f520ae27f3c
yamaneco <ykouki111228@gmail.com>
2020-09-25 06:31:16 +0900

    Add animation

---
+++ animation.py
@@ -0,0 +1,76 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+
+class AnimationMaker():
+    def __init__(self, theta1file, theta2file, l1, l2):
+        self.loadData(theta1file, theta2file)
+
+        self.l1 = l1
+        self.l2 = l2
+
+        self.trajectory_x = []
+        self.trajectory_y = []
+
+        self.figure()
+
+    def loadData(self, theta1file, theta2file):
+        theta1 = np.loadtxt(theta1file, delimiter=',', unpack=True)
+        theta2 = np.loadtxt(theta2file, delimiter=',', unpack=True)
+        self.time = theta1[0]
+        self.theta1 = theta1[1]
+        self.theta2 = theta2[1]
+
+    def figure(self):
+        self.fig, ax = plt.subplots(figsize=(10, 10))
+
+        self.line_trajectory, = ax.plot(
+            [], [], color='tab:blue', label='trajectory')
+        self.line_l1, = ax.plot(
+            [], [], color='tab:gray', label='l1', linewidth=3)
+        self.line_l2, = ax.plot(
+            [], [], color='tab:gray', label='l2', linewidth=3)
+
+        ax.set_xlabel('x [mm]')
+        ax.set_ylabel('y [mm]')
+        maxlen = self.l1 + self.l2 + 10
+        ax.set_xlim(-maxlen, maxlen)
+        ax.set_ylim(-maxlen, maxlen)
+        ax.set_aspect('equal')
+        # ax.legend(loc='lower left')
+        plt.subplots_adjust(left=0.08, right=0.97, bottom=0.04, top=0.97)
+
+    def _update(self, i):
+        theta1 = math.radians(self.theta1[i])
+        theta2 = math.radians(self.theta2[i])
+
+        x1 = self.l1 * math.cos(theta1)
+        y1 = self.l1 * math.sin(theta1)
+        x2 = x1 + self.l2 * math.cos(theta2)
+        y2 = y1 + self.l2 * math.sin(theta2)
+
+        self.trajectory_x.append(x2)
+        self.trajectory_y.append(y2)
+
+        self.line_l1.set_data([0, x1], [0, y1])
+        self.line_l2.set_data([x1, x2], [y1, y2])
+        self.line_trajectory.set_data(self.trajectory_x, self.trajectory_y)
+
+        plt.title('t = {} [s], (x, y) = ({:3.0f}, {:3.0f})'.format(
+            i / len(self.time), x2, y2))
+
+    def makeAnimation(self):
+        return animation.FuncAnimation(self.fig, self._update,
+                                       interval=100, frames=len(self.time))
+
+
+if __name__ == '__main__':
+    # animationMaker = AnimationMaker('theta1example.csv', 'theta2example.csv', 150, 150)
+    animationMaker = AnimationMaker('theta1.csv', 'theta2.csv', 150, 150)
+    ani = animationMaker.makeAnimation()
+    ani.save('animation.gif', writer='pillow')
+
+    plt.plot(animationMaker.trajectory_x, animationMaker.trajectory_y)
+    plt.savefig('a.png')
--- manipurator.py
+++ manipurator.py
@@ -2,11 +2,19 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 class Manipurator():
     def __init__(self, l1, l2, d0=0):
         self.l1 = l1
         self.l2 = l2
         self.d0 = d0
+
+    def forwardKinematics(self, theta1, theta2):
+        x1 = self.l1 * math.cos(theta1)
+        y1 = self.l1 * math.sin(theta1)
+        x2 = x1 + self.l2 * math.cos(theta2)
+        y2 = y1 + self.l2 * math.sin(theta2)
+        return x2, y2
 
     def inverseKinematics(self, x, y, eps=1e-10):
         tmp = (self.l1**2 + x**2 + (y - self.d0)**2 - self.l2**2) / \
@@ -21,6 +29,7 @@ class Manipurator():
 
         return theta1, theta2
 
+
 class PathPlanner():
     def __init__(self, acc, vmax, dt=0.01):
         self.acc = acc
@@ -29,7 +38,7 @@ class PathPlanner():
 
     def distance(self, x1, y1, x2, y2):
         return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
-        
+
     def angle(self, x1, y1, x2, y2):
         return math.atan2(y2 - y1, x2 - x1)
 
@@ -47,14 +56,16 @@ class PathPlanner():
         tList, xList, yList, vList = [0], [x1], [y1], [0]
         while diff > 0.1:
             t += self.dt
-            if t > 2: break
+            if t > 2:
+                break
             print(diff0 - distance)
 
             if diff0 - distance < self.vmax ** 2 / self.acc / 2:
                 v -= self.acc * self.dt
             elif v < self.vmax:
                 v += self.acc * self.dt
-            if v < 0: break
+            if v < 0:
+                break
             v = np.clip(v, 0, self.vmax)
 
             x += v * self.dt * math.cos(angle)
@@ -71,15 +82,20 @@ class PathPlanner():
 
         return {'t': tList, 'x': xList, 'y': yList, 'v': vList}
 
+
 if __name__ == '__main__':
     manipurator = Manipurator(150, 150)
     vmax = 308.7904831758910
     acc = vmax / 0.2
     path = PathPlanner(acc, vmax).planning(300, 0, 155, 200)
+    # path = PathPlanner(acc, vmax).planning(100, 50, 300, 150)
 
     theta1List, theta2List = [], []
     for i in range(len(path['t'])):
-        theta1, theta2 = manipurator.inverseKinematics(path['x'][i], path['y'][i])
+        theta1, theta2 = manipurator.inverseKinematics(
+            path['x'][i], path['y'][i])
+        x, y = manipurator.forwardKinematics(theta1, theta2)
+        print(x, y)
         theta1List.append(math.degrees(theta1))
         theta2List.append(math.degrees(theta2))
 
@@ -89,7 +105,7 @@ if __name__ == '__main__':
     np.savetxt('theta2.csv', theta2, delimiter=',', fmt='%04f')
 
     plt.figure()
-    plt.plot([300, 155], [0, 200])
+    # plt.plot([300, 155], [0, 200])
     plt.plot(path['x'], path['y'])
     plt.savefig('path.png')
 
